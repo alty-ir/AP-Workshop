ca8c76fb95817e058fdf2b2dd4c14d42adc1da3e
Ali Tabesh <ali.tabesh76@gmail.com>
2021-04-02 23:34:20 +0430

    Session1 Added

---
+++ Session1/Check_Coprime_Integers.java
@@ -0,0 +1,28 @@
+package Session1;
+
+import java.util.Scanner;
+
+public class Check_Coprime_Integers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first integer: ");
+        int first = scanner.nextInt();
+        System.out.println("Enter second integer: ");
+        int second = scanner.nextInt();
+        int i;
+        if (first > second)
+            i = first;
+        else
+            i = first;
+
+        while ((first % i != 0) || (second % i != 0)) {
+            i--;
+        }
+        // System.out.println(i);
+        if (i != 1)
+            System.out.println("Coprime integers");
+        else
+            System.out.println("Not-Coprime integers");
+    }
+}
---
+++ Session1/Multiplication_Table.java
@@ -0,0 +1,12 @@
+package Session1;
+
+public class Multiplication_Table {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 10; i++) {            
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i*j+" ");
+            }
+        }
+    }
+}
